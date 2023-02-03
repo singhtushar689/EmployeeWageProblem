9db9842145aef10788a2fc26df9e8b760e74e3bb
singhtushar689 <singhtushar689@gmail.com>
2023-02-03 14:15:51 +0530

    [Tushar] add: Employee Wage For A Month

--- EmployeeWageProblem/Attendance.cs
+++ EmployeeWageProblem/Attendance.cs
@@ -11,27 +11,37 @@ namespace EmployeeWageProblem
         public void CheckAttendance()
         {
             int empHrs = 0, dailyEmpWage; 
-            const int wagePerHour = 20;
-
-            Random random = new Random();
-            int checkOption = random.Next(1, 4);
-            switch (checkOption)                      //using Switch Statement
-            {
-                case 1:
-                    empHrs = 8;
-                    Console.WriteLine("Employee is present Full-Time");
-                    break;
-                case 2:
-                    empHrs = 4;
-                    Console.WriteLine("Employee is Present Part-Time");
-                    break;
-                case 3:
-                    empHrs = 0;
-                    Console.WriteLine("Employee is absent");
-                    break;
-            }
-            dailyEmpWage = (wagePerHour * empHrs);
-            Console.WriteLine("DailyWage is:" + dailyEmpWage);
+            const int wagePerHour = 20; const int maxDaysInMonth = 20;
+            int totalWage = 0;int day = 0;
+            
+           for(int i = 1; i <= maxDaysInMonth; i++)
+           {
+                Random random = new Random();
+                int checkOption = random.Next(1, 4);
+                switch (checkOption)
+                {
+                    case 1:
+                        empHrs = 8;
+                        Console.WriteLine("Employee is present Full-Time");
+                        break;
+                    case 2:
+                        empHrs = 4;
+                        Console.WriteLine("Employee is Present Part-Time");
+                        break;
+                    case 3:
+                        empHrs = 0;
+                        Console.WriteLine("Employee is absent");
+                        break;
+                }
+                day++;
+                Console.WriteLine("Day" + day);
+                dailyEmpWage = (wagePerHour * empHrs);
+                Console.WriteLine("DailyWage is:" + dailyEmpWage);
+                totalWage += dailyEmpWage;
+                Console.WriteLine("Total Emp Wage is:" + totalWage);
+           }
+          
+           
         }
     }
 }

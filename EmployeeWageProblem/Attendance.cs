77032186e821d61d181932d46cf91fa0ec38b88c
singhtushar689 <singhtushar689@gmail.com>
2023-02-03 10:55:30 +0530

    [Tushar] add: Check Whether Employee is present or not

---
+++ EmployeeWageProblem/Attendance.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageProblem
+{
+    public class Attendance
+    {
+        public void CheckAttendance()
+        {
+            int present = 1;
+            int absent = 2;
+            Random random = new Random();
+            int checkOption = random.Next(1, 3);
+            switch (checkOption)
+            {
+                case 1:
+                    Console.WriteLine("Employee is present");
+                    break;
+                case 2:
+                    Console.WriteLine("Employee is absent");
+                    break;
+            }
+
+        }
+    }
+}
---
+++ EmployeeWageProblem/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageProblem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Attendance attendance = new Attendance();
+            attendance.CheckAttendance();
+            Console.ReadLine();
+        }
+    }
+}

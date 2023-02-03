35ed2fe8ec48c846a0f5d3740a1502d61b8f1e8b
singhtushar689 <singhtushar689@gmail.com>
2023-02-03 11:05:48 +0530

    [Tushar] add:Computing Daily Wage For Employee

--- EmployeeWageProblem/Attendance.cs
+++ EmployeeWageProblem/Attendance.cs
@@ -10,20 +10,24 @@ namespace EmployeeWageProblem
     {
         public void CheckAttendance()
         {
-            int present = 1;
-            int absent = 2;
+            int present = 1, absent = 2, empHrs = 0,dailyEmpWage; ;
+           const int wagePerHour = 20;
+           
             Random random = new Random();
             int checkOption = random.Next(1, 3);
             switch (checkOption)
             {
                 case 1:
+                    empHrs = 8;
                     Console.WriteLine("Employee is present");
                     break;
                 case 2:
+                    empHrs = 0;
                     Console.WriteLine("Employee is absent");
                     break;
             }
-
+            dailyEmpWage = (wagePerHour * empHrs);
+            Console.WriteLine("DailyWage is:" + dailyEmpWage);
         }
     }
 }

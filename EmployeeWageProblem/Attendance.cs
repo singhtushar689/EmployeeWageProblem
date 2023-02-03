9819715c62d1da4bf8c6a8332b57cad637e6cd4f
singhtushar689 <singhtushar689@gmail.com>
2023-02-03 11:20:53 +0530

    [Tushar] add: Adding Part-Time

--- EmployeeWageProblem/Attendance.cs
+++ EmployeeWageProblem/Attendance.cs
@@ -10,20 +10,24 @@ namespace EmployeeWageProblem
     {
         public void CheckAttendance()
         {
-            int present = 1, absent = 2, empHrs = 0,dailyEmpWage; ;
-           const int wagePerHour = 20;
-           
+            int present = 1, absent = 2, empHrs = 0, dailyEmpWage; ;
+            const int wagePerHour = 20;
+
             Random random = new Random();
-            int checkOption = random.Next(1, 3);
+            int checkOption = random.Next(1, 4);
             switch (checkOption)
             {
                 case 1:
                     empHrs = 8;
-                    Console.WriteLine("Employee is present");
+                    Console.WriteLine("Employee is present Full-Time");
                     break;
                 case 2:
-                    empHrs = 0;
+                    empHrs = 4;
                     Console.WriteLine("Employee is absent");
+                    break;
+                case 3:
+                    empHrs = 4;
+                    Console.WriteLine("Employee is Present Half-Time");
                     break;
             }
             dailyEmpWage = (wagePerHour * empHrs);
